8fc637d3c21c65d66a314710dea564c6280a9742
Skylark92 <skylark92@live.co.kr>
2024-03-04 21:09:49 +0900

    Variable Escape Sequence

---
+++ EscapeSequence/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <Turboc.h>
+
+int main()
+{
+    // printf("\a\n"); // 알림 소리
+    printf("A");
+    printf("\tA\b\n");
+    printf("\x11\n");
+    printf("\\, \', \", \?, \111\n");
+    return 0;
+}
