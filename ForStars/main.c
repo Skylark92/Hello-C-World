8a5021a28e1598d6558d7027cbc600eebdd1ca07
Skylark92 <skylark92@live.co.kr>
2024-03-20 21:14:20 +0900

    For 7

---
+++ ForStars/main.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i, j;
+
+    for(i = 15; i > 0; i--) {
+        for (j = 1; j < i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < 15; i++) {
+        for(j = 14; j > 0; j--) {
+            if (i >= j) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    int k = 30;
+
+    for (i = 0; i < (k / 2) + 1; i++) {
+        for (j = 0; j < k; j++) {
+            if (j > (k / 2) - i && j < (k / 2) + i) {
+                printf("*");
+            } else {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
