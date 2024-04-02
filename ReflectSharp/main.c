b6ad95ee31e302fdd85435b3e35f9e9327809395
Skylark92 <skylark92@live.co.kr>
2024-04-02 20:56:29 +0900

    ReflectSharp 샾 그리기

--- ReflectSharp/main.c
+++ ReflectSharp/main.c
@@ -4,20 +4,34 @@
 
 int main()
 {
-    int i;
+    int i, j = 0;
     char brick = '@';
 
-    // 벽 그리기
-    // 우측 세로벽
-    for (i = 0; i <20; i++) {
-        gotoxy(40, i);
-        printf("%c", brick);
-    }
+    while (1) {
+        // 화면 초기화
+        clrscr();
 
-    // 하측 가로벽
-    gotoxy(0, 20);
-    for (i = 0; i < 40; i++) {
-        printf("%c", brick);
+        // 벽 그리기
+        // 우측 세로벽
+        for (i = 0; i <20; i++) {
+            gotoxy(40, i);
+            printf("%c", brick);
+        }
+
+        // 하측 가로벽
+        gotoxy(0, 20);
+        for (i = 0; i < 40; i++) {
+            printf("%c", brick);
+        }
+
+        // 커서 원점
+        gotoxy(j, j);
+        printf("#");
+
+        j++;
+        delay(300);
+
+        if (j == 19) break;
     }
 
     return 0;
