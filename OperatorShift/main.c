7b1cc3937e5a9c21c2e3678f1916fe28add8b225
Skylark92 <skylark92@live.co.kr>
2024-04-10 10:08:09 +0900

    OperatorShift 배수 예제 추가

--- OperatorShift/main.c
+++ OperatorShift/main.c
@@ -3,11 +3,33 @@
 
 int main()
 {
-    int i;
+    int i, j, result;
 
-    printf("정수를 입력하세요 : ");
+    printf("정수를 입력하세요 : \n");
     scanf("%d", &i);
-    printf("결과 = %d\n", i << 1);
+
+    printf("배수를 입력하세요 : (3, 9, 15, 60)\n");
+    scanf("%d", &j);
+
+    switch (j) {
+    case 3:
+        result = (i << 1) + i;
+        break;
+    case 9:
+        result = (i << 3) + i;
+        break;
+    case 15:
+        result = (i << 4) - i;
+        break;
+    case 60:
+        result = (i << 6) - (i << 2);
+        break;
+    default:
+        printf("잘못된 입력입니다.\n");
+        break;
+    }
+
+    printf("결과 = %d\n", result);
 
     return 0;
 }
