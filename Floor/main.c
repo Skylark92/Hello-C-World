ad822ac55cb3ed4f4cb046ad0c18df5d4bb5960f
Skylark92 <skylark92@live.co.kr>
2024-05-14 13:49:58 +0900

    Floor

---
+++ Floor/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+    printf("floor(3.14) = %f\n", floor(3.14));
+    printf("ceil(3.14) = %f\n", ceil(3.14));
+    printf("floor(-3.14) = %f\n", floor(-3.14));
+    printf("ceil(-3.14) = %f\n", ceil(-3.14));
+
+    return 0;
+}
