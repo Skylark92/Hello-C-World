3420728029a4e351288e536903f5ebd0b180ca63
Skylark92 <skylark92@live.co.kr>
2024-05-23 22:06:10 +0900

    Array Bound

---
+++ ArrayBound/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char name[10];
+
+    printf("이름이 뭐니? ");
+    gets(name);
+    printf("너가 바로 그 유명한 %s이구나.\n", name);
+    return 0;
+}
