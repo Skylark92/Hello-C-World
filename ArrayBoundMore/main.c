f60e7fb95b4b90f0534e54d054b04ca013f254d2
Skylark92 <skylark92@live.co.kr>
2024-05-24 20:56:35 +0900

    Array Bound 2

---
+++ ArrayBoundMore/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int i;
+    int ar[5];
+
+    i = 1234;
+    ar[5] = 5678;
+    printf("i = %d\n", i);
+    printf("ar[5] = %d\n", ar[5]);
+    printf("ar[6] = %d\n", ar[6]);
+
+    return 0;
+}
