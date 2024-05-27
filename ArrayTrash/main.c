64896b0d2a7984d01bce098ced8947f671a63c1b
Skylark92 <skylark92@live.co.kr>
2024-05-27 21:00:22 +0900

    Array Garbage Value

---
+++ ArrayTrash/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ar[5];
+    int i;
+
+    for (i = 0; i < 5; i++) {
+        printf("ar[%d] = %d\n", i, ar[i]);
+    }
+    return 0;
+}
