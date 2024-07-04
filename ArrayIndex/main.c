73ef8e7e7ba462d441e3a30c1030dbf293f912b2
Skylark92 <skylark92@live.co.kr>
2024-07-04 20:41:31 +0900

    Array Pointer 3

---
+++ ArrayIndex/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int ar[5] = { 1, 2, 3, 4, 5 };
+
+    printf("ar[2] = %d\n", ar[2]);
+    printf("ar[2] = %d\n", *(ar+2));
+    printf("ar[2] = %d\n", 2[ar]);
+
+    return 0;
+}
