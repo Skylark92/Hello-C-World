48116b473a11e270bebaedd02fb18d7868b35a4f
Skylark92 <skylark92@live.co.kr>
2024-07-22 22:09:28 +0900

    String 1

---
+++ strncpy/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    char str1[10] = "abcdefghi";
+    char str2[10] = "123456789";
+
+    strncpy(str2, str1, 3);
+    puts(str2);
+    return 0;
+}
